ec67578b0e79d61d4e839aa3a54c3033135ce1d9
Arduino Di Giosia <info@rmsoft.net>
2016-08-04 21:02:36 +0100

    temp

--- src/boxStatus/Modules/ConsoleModule.php
+++ src/boxStatus/Modules/ConsoleModule.php
@@ -1,24 +1,20 @@
 <?php
 namespace boxStatus\Modules;
 
+use boxStatus\Controllers\Ancestor;
 
-Class ConsoleModule
+Class ConsoleModule extends Ancestor
 {
     var $system = [];
     var $network = [];
 
+    public function __construct()
+    {
+    }
+
     public function getNetwork()
     {
-        $command = escapeshellarg("hostnamectl");
-        $a = exec ($command, $output ,$return_var);
 
-        $this->system['hostname'] = substr($output[0], strpos($output[0], ":")+2);
-        $this->system['os'] = substr($output[6], strpos($output[6], ":")+2);
-
-
-print_r($this);
-print_r($output);
-        die;
 
 
         $command = escapeshellarg("ifconfig");
@@ -49,6 +45,26 @@ print_r($output);
 
 
     }
+
+    private function _toBeNominated() {
+        // TODO find a name to this function
+        $command = escapeshellarg("hostnamectl");
+        $a = exec ($command, $output ,$return_var);
+
+        // TODO save those data to a yml file
+        // TODO write a fuction to load this data
+        // TODO add a param to the main function to load those data and set them in the response (not defaul)
+        // TODO write a function delete and reload those data
+        // TODO add a param to the main function to fire the delete/reload
+        $this->system['box']        = substr($output[5], strpos($output[5], ":")+2);
+        $this->system['boxID']      = substr($output[3], strpos($output[3], ":")+2);
+        $this->system['bootID']     = substr($output[4], strpos($output[4], ":")+2);
+        $this->system['hostname']   = substr($output[0], strpos($output[0], ":")+2);
+        $this->system['os']         = substr($output[6], strpos($output[6], ":")+2);
+        $this->system['kernel']     = substr($output[7], strpos($output[7], ":")+2);
+
+    }
+
 
 
 }
