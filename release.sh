5d8c3490e6b7d4c14b452cc418747dfd3443a322
Arduino Di Giosia <arduino.digiosia@moreniche.com>
2017-10-16 17:25:02 +0100

    adding release file

---
+++ release.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+APP_COMPOSER_INSTALL="no";
+APP_TEST="no";
+
+# getting versions
+while getopts ":c:t:" opt
+   do
+     case $opt in
+        c ) APP_COMPOSER_INSTALL=$OPTARG;;
+        t ) APP_TEST=$OPTARG;;
+     esac
+done
+
+
+
+# preparing git to checkout
+git stash
+
+# getting the last tag and checking it out
+git pull --tags
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout tags/$latestTag
+
+
+# running composer install (if requested)
+if [ ${APP_COMPOSER_INSTALL} = "yes" ]
+then
+    echo "running composer install"
+    composer install --prefer-dist --no-dev
+fi
+
+# running composer install (if requested)
+rm -f web/app_dev.php
+rm -f web/back_dev.php
+if [ ${APP_TEST} = "no" ]
+then
+    echo "# removing the app_dev file"
+    rm -f web/app_sandbox.php
+    rm -f web/back_sandbox.php
+fi
+
+
+# clear cache
+# TODO!
+
+git describe
